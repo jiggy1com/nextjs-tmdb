245a382eba20700e5f532f798c73d5b6422f3bd0
Joe Velez <joe@jiggy1.com>
2026-01-17 14:10:34 -0500

    fixing scss and apollo client for vercel

--- app/ApolloClient.ts
+++ app/ApolloClient.ts
@@ -1,35 +1,39 @@
 // https://github.com/apollographql/apollo-client-integrations/tree/main/packages/nextjs
-import { HttpLink, createHttpLink } from "@apollo/client";
+import { HttpLink, createHttpLink } from '@apollo/client';
 import {
-	registerApolloClient,
-	ApolloClient,
-	InMemoryCache,
-} from "@apollo/client-integration-nextjs";
+    registerApolloClient,
+    ApolloClient,
+    InMemoryCache,
+} from '@apollo/client-integration-nextjs';
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
+    const link =
+        process.env.NODE_ENV === 'development'
+            ? new HttpLink({
+                  uri: 'http://localhost:3000/api/graphql',
+              })
+            :new HttpLink({
+                uri: 'http://localhost:3000/api/graphql',
+            })
 
-	const link = process.env.NODE_ENV === "development"
-	? new HttpLink({
-		uri: "http://localhost:3000/api/graphql",
-		})
-	: createHttpLink({
-			uri: "/api/graphql",
-		});
+            // createHttpLink({
+    //       uri: '/api/graphql',
+    //   });
 
-	return new ApolloClient({
-		cache: new InMemoryCache(),
-		link,
-		// link: createHttpLink({
-		// 	uri: "/api/graphql",
-		// }),
-		// link: new HttpLink({
-		// 	// this needs to be an absolute url, as relative urls cannot be used in SSR
-		// 	uri: "http://localhost:3000/api/graphql",
-		// 	fetchOptions: {
-		// 		// you can pass additional options that should be passed to `fetch` here,
-		// 		// e.g. Next.js-related `fetch` options regarding caching and revalidation
-		// 		// see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
-		// 	},
-		// }),
-	});
+    return new ApolloClient({
+        cache: new InMemoryCache(),
+        link,
+        // link: createHttpLink({
+        // 	uri: "/api/graphql",
+        // }),
+        // link: new HttpLink({
+        // 	// this needs to be an absolute url, as relative urls cannot be used in SSR
+        // 	uri: "http://localhost:3000/api/graphql",
+        // 	fetchOptions: {
+        // 		// you can pass additional options that should be passed to `fetch` here,
+        // 		// e.g. Next.js-related `fetch` options regarding caching and revalidation
+        // 		// see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
+        // 	},
+        // }),
+    });
 });
