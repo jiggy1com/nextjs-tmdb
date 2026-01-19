338644792d775a15f31a9050a8017226a5f94d1c
Joe Velez <joe@jiggy1.com>
2026-01-19 14:17:05 -0500

    adding top rated and upcoming graphql schema, resolver

--- app/movie/top-rated/page.tsx
+++ app/movie/top-rated/page.tsx
@@ -1,3 +1,35 @@
-export default function TopRatedPage() {
-    return <div>top rated page</div>;
+import { getClient } from '@/app/ApolloClient';
+import { GetTopRatedDocument } from '@/app/api/graphql/generated/graphql';
+import { Heading } from '@components/text/Heading';
+import { Container } from '@components/container/Container';
+
+export default async function MoviesTopRatedPage() {
+    const result = await getClient()
+        .query({
+            query: GetTopRatedDocument,
+            fetchPolicy: 'no-cache',
+        })
+        .catch((err) => {
+            console.log('MoviesTopRatedPage:err', err);
+            return {
+                result: {
+                    data: {
+                        getTopRated: {
+                            results: [],
+                            total_pages: 0,
+                            total_results: 0,
+                        },
+                    },
+                },
+            };
+        });
+    const data = { result };
+
+    return (
+        <Container>
+            <Heading as={'h1'}>Top Rated Movies</Heading>
+            SERVER side API Data:
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </Container>
+    );
 }
